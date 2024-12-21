37c4f0d23ab973abef1ca5cd6ee3189ea362631d
Thomas Hooge <thomas@hoogi.de>
2024-12-21 21:05:03 +0100

    Remove accidentially created duplicate code

--- lib/obp60task/obp60task.cpp
+++ lib/obp60task/obp60task.cpp
@@ -299,13 +299,6 @@ void OBP60Task(GwApi *api){
         doImageRequest(api, imgformat, filename, request);
     });
 
-    String imgformat = config->getConfigItem(config->imageFormat,true)->asString();
-    String filename = "screen." + imgformat;
-    filename.toLowerCase();
-    api->registerRequestHandler(filename, [api, imgformat, filename](AsyncWebServerRequest *request) {
-        doImageRequest(api, imgformat, filename, request);
-    });
-
     tN2kMsg N2kMsg;
 
     LOG_DEBUG(GwLog::LOG,"obp60task started");
