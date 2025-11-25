f77107616d78e7f379f7b2b759b09d21b2b4c135
norbert-walter <norbert-walter@web.de>
2025-11-25 18:02:40 +0100

    Add new PageNavigation (not complete)

---
+++ lib/obp60task/PageNavigation.cpp
@@ -0,0 +1,142 @@
+#if defined BOARD_OBP60S3 || defined BOARD_OBP40S3
+
+#include "Pagedata.h"
+#include "OBP60Extensions.h"
+
+class PageNavigation : public Page
+{
+public:
+    PageNavigation(CommonData &common){
+        commonData = &common;
+        common.logger->logDebug(GwLog::LOG,"Instantiate PageNavigation");
+    }
+
+    virtual int handleKey(int key){
+        // Code for keylock
+        if(key == 11){
+            commonData->keylock = !commonData->keylock;
+            return 0;                   // Commit the key
+        }
+        return key;
+    }
+
+    int displayPage(PageData &pageData){
+        GwConfigHandler *config = commonData->config;
+        GwLog *logger = commonData->logger;
+
+        // Old values for hold function
+        static String svalue1old = "";
+        static String unit1old = "";
+        static String svalue2old = "";
+        static String unit2old = "";
+        static String svalue3old = "";
+        static String unit3old = "";
+        static String svalue4old = "";
+        static String unit4old = "";
+
+        // Get config data
+        String lengthformat = config->getString(config->lengthFormat);
+        // bool simulation = config->getBool(config->useSimuData);
+        bool holdvalues = config->getBool(config->holdvalues);
+        String flashLED = config->getString(config->flashLED);
+        String backlightMode = config->getString(config->backlight);
+        
+        // Get boat values #1
+        GwApi::BoatValue *bvalue1 = pageData.values[0]; // First element in list (only one value by PageOneValue)
+        String name1 = xdrDelete(bvalue1->getName());   // Value name
+        name1 = name1.substring(0, 6);                  // String length limit for value name
+        double value1 = bvalue1->value;                 // Value as double in SI unit
+        bool valid1 = bvalue1->valid;                   // Valid information 
+        String svalue1 = formatValue(bvalue1, *commonData).svalue;    // Formatted value as string including unit conversion and switching decimal places
+        String unit1 = formatValue(bvalue1, *commonData).unit;        // Unit of value
+
+        // Get boat values #2
+        GwApi::BoatValue *bvalue2 = pageData.values[1]; // Second element in list (only one value by PageOneValue)
+        String name2 = xdrDelete(bvalue2->getName());   // Value name
+        name2 = name2.substring(0, 6);                  // String length limit for value name
+        double value2 = bvalue2->value;                 // Value as double in SI unit
+        bool valid2 = bvalue2->valid;                   // Valid information 
+        String svalue2 = formatValue(bvalue2, *commonData).svalue;    // Formatted value as string including unit conversion and switching decimal places
+        String unit2 = formatValue(bvalue2, *commonData).unit;        // Unit of value
+
+        // Get boat values #3
+        GwApi::BoatValue *bvalue3 = pageData.values[2]; // Second element in list (only one value by PageOneValue)
+        String name3 = xdrDelete(bvalue3->getName());   // Value name
+        name3 = name3.substring(0, 6);                  // String length limit for value name
+        double value3 = bvalue3->value;                 // Value as double in SI unit
+        bool valid3 = bvalue3->valid;                   // Valid information 
+        String svalue3 = formatValue(bvalue3, *commonData).svalue;    // Formatted value as string including unit conversion and switching decimal places
+        String unit3 = formatValue(bvalue3, *commonData).unit;        // Unit of value
+
+        // Get boat values #4
+        GwApi::BoatValue *bvalue4 = pageData.values[3]; // Second element in list (only one value by PageOneValue)
+        String name4 = xdrDelete(bvalue4->getName());   // Value name
+        name4 = name4.substring(0, 6);                  // String length limit for value name
+        double value4 = bvalue4->value;                 // Value as double in SI unit
+        bool valid4 = bvalue4->valid;                   // Valid information 
+        String svalue4 = formatValue(bvalue4, *commonData).svalue;    // Formatted value as string including unit conversion and switching decimal places
+        String unit4 = formatValue(bvalue4, *commonData).unit;        // Unit of value
+
+        // Optical warning by limit violation (unused)
+        if(String(flashLED) == "Limit Violation"){
+            setBlinkingLED(false);
+            setFlashLED(false); 
+        }
+
+        // Logging boat values
+        if (bvalue1 == NULL) return PAGE_OK; // WTF why this statement?
+        LOG_DEBUG(GwLog::LOG,"Drawing at PageNavigation, %s: %f, %s: %f, %s: %f, %s: %f", name1.c_str(), value1, name2.c_str(), value2, name3.c_str(), value3, name4.c_str(), value4);
+
+        // Draw page
+        //***********************************************************
+
+        // Set display in partial refresh mode
+        getdisplay().setPartialWindow(0, 0, getdisplay().width(), getdisplay().height()); // Set partial update
+
+        getdisplay().setTextColor(commonData->fgcolor);
+
+        // ############### Draw Navigation Map ################
+        getdisplay().setFont(&Ubuntu_Bold12pt8b);
+
+        getdisplay().setCursor(20, 60);
+        getdisplay().print(name1);
+        getdisplay().setCursor(120, 60);
+        getdisplay().print(svalue1);
+
+        getdisplay().setCursor(20, 80);
+        getdisplay().print(name2);
+        getdisplay().setCursor(120, 80);
+        getdisplay().print(svalue2);
+
+        getdisplay().setCursor(20, 100);
+        getdisplay().print(name3);
+        getdisplay().setCursor(120, 100);
+        getdisplay().print(svalue3);
+
+        getdisplay().setCursor(20, 120);
+        getdisplay().print(name4);
+        getdisplay().setCursor(120, 120);
+        getdisplay().print(svalue4);
+
+        return PAGE_UPDATE;
+    };
+};
+
+static Page *createPage(CommonData &common){
+    return new PageNavigation(common);
+}/**
+ * with the code below we make this page known to the PageTask
+ * we give it a type (name) that can be selected in the config
+ * we define which function is to be called
+ * and we provide the number of user parameters we expect
+ * this will be number of BoatValue pointers in pageData.values
+ */
+PageDescription registerPageNavigation(
+    "Navigation",       // Page name
+    createPage,         // Action
+    0,                  // Number of bus values depends on selection in Web configuration
+    {"LAT","LON","HDT","SOG"},  // Bus values we need in the page
+    true                // Show display header on/off
+);
+
+#endif
